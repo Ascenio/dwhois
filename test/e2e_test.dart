a4917bac80a670ddeef6b4adaec725dc0cb078b6
Ascênio Sanderson <ascenio@hotmail.com>
2022-03-14 00:10:58 -0300

    fix: reduces volatile mock data in e2e tests

--- test/e2e_test.dart
+++ test/e2e_test.dart
@@ -67,25 +67,6 @@ const googleResultBegin = '''
    Registry Domain ID: 2138514_DOMAIN_COM-VRSN\r
    Registrar WHOIS Server: whois.markmonitor.com\r
    Registrar URL: http://www.markmonitor.com\r
-   Updated Date: 2019-09-09T15:39:04Z\r
-   Creation Date: 1997-09-15T04:00:00Z\r
-   Registry Expiry Date: 2028-09-14T04:00:00Z\r
-   Registrar: MarkMonitor Inc.\r
-   Registrar IANA ID: 292\r
-   Registrar Abuse Contact Email: abusecomplaints@markmonitor.com\r
-   Registrar Abuse Contact Phone: +1.2083895740\r
-   Domain Status: clientDeleteProhibited https://icann.org/epp#clientDeleteProhibited\r
-   Domain Status: clientTransferProhibited https://icann.org/epp#clientTransferProhibited\r
-   Domain Status: clientUpdateProhibited https://icann.org/epp#clientUpdateProhibited\r
-   Domain Status: serverDeleteProhibited https://icann.org/epp#serverDeleteProhibited\r
-   Domain Status: serverTransferProhibited https://icann.org/epp#serverTransferProhibited\r
-   Domain Status: serverUpdateProhibited https://icann.org/epp#serverUpdateProhibited\r
-   Name Server: NS1.GOOGLE.COM\r
-   Name Server: NS2.GOOGLE.COM\r
-   Name Server: NS3.GOOGLE.COM\r
-   Name Server: NS4.GOOGLE.COM\r
-   DNSSEC: unsigned\r
-   URL of the ICANN Whois Inaccuracy Complaint Form: https://www.icann.org/wicf/\r
 ''';
 
 const pubDevResultBegin = '''
