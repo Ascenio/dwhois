fc0c5ea4c787be1687b4503db0465f1038e455ef
Ascênio Sanderson <ascenio@hotmail.com>
2022-03-13 22:15:28 -0300

    feat: working basic example

--- bin/dwhois.dart
+++ bin/dwhois.dart
@@ -1,3 +1,12 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'dart:io';
+
+Future<void> main() async {
+  final domain = 'google.com.br';
+  final addresses = await InternetAddress.lookup('whois.registro.br');
+  print(addresses);
+  final address = addresses.first;
+  final socket = await Socket.connect(address, 43);
+  socket.add('$domain\r\n'.codeUnits);
+  final body = await socket.map((bytes) => String.fromCharCodes(bytes)).first;
+  print(body);
 }
