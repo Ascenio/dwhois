086293598e8453fcbc78b727877805aeda057935
Ascênio Sanderson <ascenio@hotmail.com>
2022-04-15 17:44:17 -0300

    refactor: simplifies `hasResult` matcher

--- test/matchers/has_result.dart
+++ test/matchers/has_result.dart
@@ -1,12 +1,8 @@
 import 'package:dwhois/dwhois.dart';
 import 'package:test/test.dart';
 
-Matcher hasResult(WhoisResult result) => _HasResult(startsWith(result.body));
-
-class _HasResult extends CustomMatcher {
-  _HasResult(Object? valueOrMatcher)
-      : super('Result with body like', 'body', valueOrMatcher);
-
-  @override
-  Object? featureValueOf(dynamic actual) => (actual as WhoisResult).body;
+Matcher hasResult(WhoisResult expected) {
+  return TypeMatcher<WhoisResult>()
+      .having((actual) => actual.domain, 'domain', expected.domain)
+      .having((actual) => actual.body, 'body', startsWith(expected.body));
 }
